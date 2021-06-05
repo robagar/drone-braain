0a3142e3c2f2c27db0bc5ba840c2f3dd123fedc0
Rob Agar <robagar@fastmail.net>
2021-06-05 13:24:27 +0100

    move towards / back away from face with very rough distance estimation

--- programs/follow_face.py
+++ programs/follow_face.py
@@ -24,6 +24,12 @@ async def detect_faces(drone, frame, cuda):
         finally:
             face_detected.release()
 
+def estimate_face_distance(face):
+    try:
+        return round(100 * 50000 / face.Area)
+    except Exception as e:
+        print(f'{e}')
+
 
 async def fly(drone):
     await drone.takeoff()
@@ -32,12 +38,19 @@ async def fly(drone):
             await face_detected.acquire()
             await face_detected.wait()
             print(face)
+            face_distance = estimate_face_distance(face) 
+            print(f'estimated distance: {face_distance}cm')
             x,y = face.Center
             turn_angle = video_x_to_local_azimuth(x)
         finally:
             face_detected.release()
         print(f'[drone] TURN {turn_angle}')
         await drone.turn_clockwise(turn_angle)
+
+        if face_distance < 40:
+            await drone.move_back(40)
+        if face_distance > 60:
+            await drone.move_forward(min(face_distance - 40, 100))
 
 
 run_jetson_tello_app(fly, process_frame=detect_faces)
